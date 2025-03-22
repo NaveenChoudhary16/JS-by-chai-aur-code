4e164d70469b663d4d0850398e796c1ab52de0dd
NaveenChoudhary16 <naveenchaudhary0002487@gmail.com>
2025-03-22 16:48:36 +0000

    dev container

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,11 @@
+//we avoid var as it dosent allow block and function scope
+// const=1444555
+let name="Naveen"//str=string
+let age=20//number
+let loggedIn= false//boolean
+let state;//undefined
+let value=null//null
+//bigint
+//symbol =>to find uniqueness
+
+//object=>
